766a3b1fad0e5c9e0ab7fc53bdc76de6530c6c13
SupervisedThinking <supervisedthinking@gmail.com>
2021-09-07 22:54:53 +0200

    libogg-system: updated to 1.3.5

--- packages/supervisedthinking/audio/libogg-system/package.mk
+++ packages/supervisedthinking/audio/libogg-system/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="libogg-system"
-PKG_VERSION="1.3.4"
-PKG_SHA256="c163bc12bc300c401b6aa35907ac682671ea376f13ae0969a220f7ddf71893fe"
+PKG_VERSION="1.3.5"
+PKG_SHA256="c4d91be36fc8e54deae7575241e03f4211eb102afb3fc0775fbbc1b740016705"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.xiph.org/ogg/"
 PKG_URL="http://downloads.xiph.org/releases/ogg/libogg-${PKG_VERSION}.tar.xz"
