4f74b6aaee7b30b844ed755bbc0bba3fac9cde48
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-06 20:16:52 +0200

    retroarch-joypad-autoconfig: updated to 1.9.10

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/retroarch-joypad-autoconfig/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/retroarch-joypad-autoconfig/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="1.9.9"
-PKG_SHA256="ee1f32315eda1793ebea9d88113004a24d2ffd7e9227d136c1de5f38ee5688be"
+PKG_VERSION="1.9.10"
+PKG_SHA256="c764150cd56dfe249f3454c4e81012a5a53925abd0194d663d0b4ec8afb53589"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/retroarch-joypad-autoconfig"
 PKG_URL="https://github.com/libretro/retroarch-joypad-autoconfig/archive/refs/tags/v${PKG_VERSION}.tar.gz"
