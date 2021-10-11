bdd747aafcd69505dd46b9469270dd81a52c51c2
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-11 23:24:31 +0200

    fs-uae: update to v3.1.35

--- packages/supervisedthinking/emulation/standalone/fs-uae/package.mk
+++ packages/supervisedthinking/emulation/standalone/fs-uae/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fs-uae"
-PKG_VERSION="4b03820cbeaeeaa9b06f1cfa71ee66d740fc0ba3" # 3.0.4
-PKG_SHA256="9a0347cc0663ba5c8f32a8aafc2a3688b70ecc470838d6086856396233f99814"
+PKG_VERSION="3.1.35"
+PKG_SHA256="0d868d8f3e400bae4bf75f469cc580fc49445aa0250473c64da0fb4f955ada20"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/FrodeSolheim/fs-uae"
-PKG_URL="https://github.com/FrodeSolheim/fs-uae/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/FrodeSolheim/fs-uae/archive/refs/tags/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc glib sdl2 glew-cmake libmpeg2 libXi openal-soft-system capsimg zlib libpng"
 PKG_LONGDESC="FS-UAE amiga emulator."
 PKG_TOOLCHAIN="autotools"
