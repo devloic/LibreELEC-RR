b781951fff20d0bb8da246674afbd68bf6d307d8
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-21 17:00:13 +0200

    xf86-video-*: updated descriptions, pkg sites & clean ups

--- packages/x11/driver/xf86-video-nvidia/package.mk
+++ packages/x11/driver/xf86-video-nvidia/package.mk
@@ -3,17 +3,17 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xf86-video-nvidia"
-# Remember to run "python3 packages/x11/driver/xf86-video-nvidia/scripts/make_nvidia_udev.py" and commit changes to
-# "packages/x11/driver/xf86-video-nvidia/udev.d/96-nvidia.rules" whenever bumping version.
-# Host may require installation of python3-lxml and python3-requests packages.
+# Remember to run "python3 packages/x11/driver/xf86-video-nvidia/scripts/make_nvidia_udev.py" and commit 
+# changes to "packages/x11/driver/xf86-video-nvidia/udev.d/96-nvidia.rules" whenever bumping version.
+# The build host may require installation of python3-lxml and python3-requests packages.
 PKG_VERSION="470.74"
 PKG_SHA256="b1ab5db6bffd5246fc571ef252ca4406332bd204a12e4063d3fe0939224d0b56"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
-PKG_SITE="http://www.nvidia.com/"
+PKG_SITE="https://www.nvidia.com/en-us/drivers/unix/"
 PKG_URL="http://us.download.nvidia.com/XFree86/Linux-x86_64/${PKG_VERSION}/NVIDIA-Linux-x86_64-${PKG_VERSION}-no-compat32.run"
 PKG_DEPENDS_TARGET="util-macros xorg-server libvdpau libglvnd"
-PKG_LONGDESC="The Xorg driver for NVIDIA video chips."
+PKG_LONGDESC="The Xorg driver for NVIDIA GPUs supporting the GeForce 600 Series & above."
 PKG_TOOLCHAIN="manual"
 
 PKG_IS_KERNEL_PKG="yes"
