128a24302a8bd5245fe946b794c05bf21c2c70bc
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-06 20:55:16 +0100

    gstreamer: use RR configs

--- packages/multimedia/gstreamer/gstreamer/package.mk
+++ packages/multimedia/gstreamer/gstreamer/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="27d7139ff517a52a49b91908e6bb150df930f05c3c0ea7de20624cf7be40f640"
 PKG_LICENSE="GPL-2.1-or-later"
 PKG_SITE="https://gstreamer.freedesktop.org"
 PKG_URL="https://gstreamer.freedesktop.org/src/gstreamer/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain glib glib:host"
+PKG_DEPENDS_TARGET="toolchain glib glib:host ffmpeg libvorbis-system flac-system"
 PKG_LONGDESC="GStreamer open-source multimedia framework core library"
 
 pre_configure_target() {
