1019d20f8b56fc073426e943d663bb1379037b4f
SupervisedThinking <supervisedthinking@gmail.com>
2022-03-15 14:55:01 +0100

    libglvnd: update to 1.4.0

--- packages/graphics/libglvnd/package.mk
+++ packages/graphics/libglvnd/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libglvnd"
-PKG_VERSION="1.3.4"
-PKG_SHA256="8f4218d7cdaf89d5b7eced818e810ccbc76f4bb9cba36d66eddac5a7ca892bab"
+PKG_VERSION="1.4.0"
+PKG_SHA256="1eb5c2be8d213ad5d31cfb4efbb331d42f3d9f5617c885ce7e89f572ec2bb4b8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/NVIDIA/libglvnd"
 PKG_URL="https://github.com/NVIDIA/libglvnd/archive/v${PKG_VERSION}.tar.gz"
