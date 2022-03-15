d508f700b6fd760bab9c1611e222dfb3fe495ec6
SupervisedThinking <supervisedthinking@gmail.com>
2022-03-15 14:32:09 +0100

    samples-lr: update to git 7418a58

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/samples-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/samples-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="samples-lr"
-PKG_VERSION="ecb3417f693a4712eedd122073d4281a501cb76e"
-PKG_SHA256="6078bfe63f37cf15a8604afa2b280bbbc259f29914119a2489bcfd37dad17122"
+PKG_VERSION="7418a585efd24c6506ca5f09f90c36268f0074ed"
+PKG_SHA256="81519a9efe2babf0d0c25eac8df5142fdf931eb4cf35cee755bbec8d5d8def59"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-samples"
 PKG_URL="https://github.com/libretro/libretro-samples/archive/${PKG_VERSION}.tar.gz"
@@ -21,23 +21,36 @@ configure_package() {
   if [ "${OPENGL_SUPPORT}" = "yes" ]; then
     PKG_DEPENDS_TARGET+=" ${OPENGL}"
   fi
+
+  # OpenGL ES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+
+  # Vulkan support
+  if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${VULKAN}"
+  fi
 }
 
 make_target() {
- make -C input/button_test
- make -C midi/midi_test
- make -C tests/test
+  make -C input/button_test
+  make -C midi/midi_test
+  make -C tests/test
 
- if [ "${ARCH}" = "x86_64" ]; then
-   make -C tests/test_advanced
-   make -C video/opengl/libretro_test_gl_fixedfunction
-   make -C video/opengl/libretro_test_gl_shaders
- fi
+  if [ "${ARCH}" = "x86_64" ]; then
+    make -C tests/test_advanced
+  fi
 
- if [ "${VULKAN_SUPPORT}" = "yes" ]; then
-   make -C video/vulkan/vk_rendering
-   make -C video/vulkan/vk_async_compute
- fi
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    make -C video/opengl/libretro_test_gl_fixedfunction
+    make -C video/opengl/libretro_test_gl_shaders
+  fi
+
+  if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+    make -C video/vulkan/vk_rendering
+    make -C video/vulkan/vk_async_compute
+  fi
 }
 
 makeinstall_target() {
@@ -47,9 +60,13 @@ makeinstall_target() {
   cp midi/midi_test/*.so    ${INSTALL}/usr/lib/libretro/
   cp tests/test/*.so        ${INSTALL}/usr/lib/libretro/
 
-  # Install OpenGL test cores
+  # Install Advanced test cores
   if [ "${ARCH}" = "x86_64" ]; then
     cp tests/test_advanced/*.so                         ${INSTALL}/usr/lib/libretro/
+  fi
+
+  # Install OpenGL test cores
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
     cp video/opengl/libretro_test_gl_fixedfunction/*.so ${INSTALL}/usr/lib/libretro/
     cp video/opengl/libretro_test_gl_shaders/*.so       ${INSTALL}/usr/lib/libretro/
   fi
