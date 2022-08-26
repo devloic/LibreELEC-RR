d3ec1caa2ef7c53c09c54adba0821b5ff0725c82
SupervisedThinking <supervisedthinking@gmail.com>
2022-08-26 14:08:49 +0200

    imgui: initial package v1.88

---
+++ packages/supervisedthinking/toolkit/imgui/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="imgui"
+PKG_VERSION="1.88"
+PKG_SHA256="9f14c788aee15b777051e48f868c5d4d959bd679fc5050e3d2a29de80d8fd32e"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/ocornut/imgui"
+PKG_URL="https://github.com/ocornut/imgui/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Dear ImGui: Bloat-free Graphical User interface for C++ with minimal dependencies"
