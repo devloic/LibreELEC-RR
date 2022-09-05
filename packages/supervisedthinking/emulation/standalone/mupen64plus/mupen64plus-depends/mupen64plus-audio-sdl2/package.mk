17bfab4af009e338a2c009c84aab4ef8cbb56d79
SupervisedThinking <supervisedthinking@gmail.com>
2022-09-05 02:35:01 +0200

    mupen64plus-audio-sdl2: update pkg site

--- packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-audio-sdl2/package.mk
+++ packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-audio-sdl2/package.mk
@@ -3,10 +3,10 @@
 
 PKG_NAME="mupen64plus-audio-sdl2"
 PKG_VERSION="4922bfa066c375a9b3f7957d38fb1ef04d9fe8f7"
-PKG_SHA256="78caacbaa7f7bcf8d6c96a2728a8d15bf0bbc54bf08beaf583647529b6db60eb"
+PKG_SHA256="60c7f1dd31a1dc616b67beaf86e3d28bfe7c2c9ec2c593d7db03a8ca15f01329"
 PKG_LICENSE="GPL-2.0-or-later"
-PKG_SITE="https://github.com/m64p/mupen64plus-audio-sdl2"
-PKG_URL="https://github.com/m64p/mupen64plus-audio-sdl2/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/simple64/simple64-audio-sdl2"
+PKG_URL="https://github.com/simple64/simple64-audio-sdl2/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc mupen64plus-core libsamplerate sdl2"
 PKG_LONGDESC="A low-level N64 video emulation plugin, based on the pixel-perfect angrylion RDP plugin with some improvements."
 PKG_TOOLCHAIN="make"
