965b02ec6193dbb3a9800b5672088c0c8cdd4971
SupervisedThinking <supervisedthinking@gmail.com>
2022-09-05 02:35:16 +0200

    mupen64plus-gui: update pkg site

--- packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-gui/package.mk
+++ packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-gui/package.mk
@@ -3,10 +3,10 @@
 
 PKG_NAME="mupen64plus-gui"
 PKG_VERSION="f454370d5d7d05e67317ac48fa45aefe0fdedf9f"
-PKG_SHA256="050b72cbe429ce0c1930ddedfb9d3ba644b9995b2f5a5406d83f19e3247147c4"
+PKG_SHA256="7d05c8ae69d63b13ce420c1c85a94f0eb77e1a2336a49e287714f7fac7019688"
 PKG_LICENSE="GPL-3.0-or-later"
-PKG_SITE="https://github.com/m64p/mupen64plus-gui"
-PKG_URL="https://github.com/m64p/mupen64plus-gui/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/simple64/simple64-gui"
+PKG_URL="https://github.com/simple64/simple64-gui/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc sdl2 qt5 p7zip-system libpng zlib mupen64plus-core"
 PKG_LONGDESC="mupen64plus GUI written in Qt5"
 PKG_TOOLCHAIN="manual"
