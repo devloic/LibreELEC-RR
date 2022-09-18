4eeb255722a1697a53442813a1cb2d017c7eaf13
SupervisedThinking <supervisedthinking@gmail.com>
2022-09-18 22:02:54 +0200

    xpadneo: update to 0.9.5

--- packages/supervisedthinking/linux-drivers/xpadneo/package.mk
+++ packages/supervisedthinking/linux-drivers/xpadneo/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="xpadneo"
-PKG_VERSION="0.9.4"
-PKG_SHA256="6682feaf0ba730b08d36f6d0fb091ca480bbbebf0d4ea796abf78e872b15096f"
+PKG_VERSION="0.9.5"
+PKG_SHA256="7518f75d7d30ae1c10ff110e7b066907a7e2c4586a670441d7c4bac9fc7afd52"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://atar-axis.github.io/xpadneo/"
 PKG_URL="https://github.com/atar-axis/xpadneo/archive/refs/tags/v${PKG_VERSION}.tar.gz"
