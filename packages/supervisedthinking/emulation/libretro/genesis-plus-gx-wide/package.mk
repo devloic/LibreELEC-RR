aba0549cd4e39b68b41b194301316a30512df133
SupervisedThinking <supervisedthinking@gmail.com>
2022-10-18 18:59:36 +0200

    genesis-plus-gx-wide: update to git f634cc5

--- packages/supervisedthinking/emulation/libretro/genesis-plus-gx-wide/package.mk
+++ packages/supervisedthinking/emulation/libretro/genesis-plus-gx-wide/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="genesis-plus-gx-wide"
-PKG_VERSION="fa71a1640f6a01025a045b616007c2fe4effd384"
-PKG_SHA256="4ed8aba9c9a93ac4026ac94a9d5d8193b2f7c9ee526e91c28d235303e4b44cf7"
+PKG_VERSION="f634cc53fd8dd8c09a24aac5314d147635857e62"
+PKG_SHA256="93ae52d407df51908f0b9cae28d29a9652e7a6b0ac4577b8051a5c262dba012a"
 PKG_LICENSE="Modified BSD / LGPL-2.1-or-later"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX-Wide"
 PKG_URL="https://github.com/libretro/Genesis-Plus-GX-Wide/archive/${PKG_VERSION}.tar.gz"
