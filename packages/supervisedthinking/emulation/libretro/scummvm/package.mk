891ca057a2cc2cf612aae56a904030b5928b6949
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-07 17:50:09 +0100

    scummvm: update to 2.6.1
    - switched repo to use mainline scummvm + updated libretro backend
    - updated build options / rebuild aux data
    - added aux data to build

--- packages/supervisedthinking/emulation/libretro/scummvm/package.mk
+++ packages/supervisedthinking/emulation/libretro/scummvm/package.mk
@@ -2,14 +2,16 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="scummvm"
-PKG_VERSION="2fb2e4c551c9c1510c56f6e890ee0300b7b3fca3"
-PKG_SHA256="3f9aca170e80dbd167eab6e39f91042447a95d84c0345fd953fefcd1a562b327"
+PKG_VERSION="7cf738d27095c072c253bef45445e63f708a5441" # v2.6.1
 PKG_LICENSE="GPL-3.0-or-later"
-PKG_SITE="https://github.com/libretro/scummvm"
-PKG_URL="https://github.com/libretro/scummvm/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/SupervisedThinking/scummvm"
+PKG_URL="https://github.com/SupervisedThinking/scummvm.git"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="ScummVM is an interpreter for point-and-click adventure games that can be used as a libretro core."
 PKG_TOOLCHAIN="make"
+PKG_GIT_CLONE_BRANCH="branch-2-6-1-libretro"
+PKG_GIT_CLONE_SINGLE="yes"
+GET_HANDLER_SUPPORT="git"
 PKG_BUILD_FLAGS="-sysroot +speed"
 
 PKG_LIBNAME="scummvm_libretro.so"
@@ -32,12 +34,22 @@ pre_make_target() {
   # Fix build path
   cd ${PKG_BUILD}
 
-  CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
-  export AR+=" cru"
-  export LD="${CC}"
+  if [ ! "${ARCH}" = "x86_64" ]; then
+    CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
+  fi
 }
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}
+
+post_makeinstall_target() {
+  if [ -f ${PKG_BUILD}/backends/platform/libretro/aux-data/scummvm.zip ]; then
+    echo -e "\n### Rebuild ScummVM auxiliary data ####\n"
+    cd ${PKG_BUILD}/backends/platform/libretro/aux-data
+    ./bundle_aux_data.bash
+    mkdir -p ${INSTALL}/usr/share/retroarch/system
+      cp -v ${PKG_BUILD}/backends/platform/libretro/aux-data/scummvm.zip ${INSTALL}/usr/share/retroarch/system/
+  fi
 }
