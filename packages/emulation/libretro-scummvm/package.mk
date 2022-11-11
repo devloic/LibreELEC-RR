7ef6fc40fa5083d5dace6dcd18e951747582a163
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-11 13:25:46 +0100

    libretro-scummvm: update to 2.6.1
    - updated repo to use mainline ScummVM + libretro-scummvm-backend by diablodiab
    - updated build opts

--- packages/emulation/libretro-scummvm/package.mk
+++ packages/emulation/libretro-scummvm/package.mk
@@ -2,26 +2,47 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-scummvm"
-PKG_VERSION="80cb7269a33b233dcea27d8d01df084b0d35c80a"
-PKG_SHA256="d35e8bc65e98d530480b4ad13cfed4d0b9c41c32f5de06a360181d8b1c7bda7b"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/scummvm"
-PKG_URL="https://github.com/libretro/scummvm/archive/${PKG_VERSION}.tar.gz"
+PKG_VERSION="2.6.1-libretro"
+PKG_SHA256="7a3fb7fa744e6326b65b2383adcc1935a7b1cf4eafd565bbdb9e1228c58e8b79"
+PKG_LICENSE="GPL-3.0-or-later"
+PKG_SITE="https://github.com/SupervisedThinking/scummvm"
+PKG_URL="https://github.com/SupervisedThinking/scummvm/releases/download/v${PKG_VERSION}/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform"
 PKG_LONGDESC="game.libretro.scummvm: scummvm for Kodi"
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+speed"
 
 PKG_LIBNAME="scummvm_libretro.so"
 PKG_LIBPATH="backends/platform/libretro/build/${PKG_LIBNAME}"
 PKG_LIBVAR="SCUMMVM_LIB"
 
-PKG_MAKE_OPTS_TARGET="-C backends/platform/libretro/build/"
+PKG_MAKE_OPTS_TARGET="-C backends/platform/libretro/build GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+    # ARM NEON support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+    PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+  fi
+}
 
 pre_make_target() {
+  # Rebuild ScummVM auxiliary data
+  if [ -f ${PKG_BUILD}/backends/platform/libretro/aux-data/scummvm.zip ]; then
+    echo -e "\n### Rebuild ScummVM auxiliary data ####\n"
+    cd ${PKG_BUILD}/backends/platform/libretro/aux-data
+    ./bundle_aux_data.bash
+  fi
+
+  # Fix build path
   cd ${PKG_BUILD}
-  CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
-  export AR+=" cru"
-  export LD="${CC}"
+
+  if [ ! "${ARCH}" = "x86_64" ]; then
+    CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
+  fi
 }
 
 makeinstall_target() {
