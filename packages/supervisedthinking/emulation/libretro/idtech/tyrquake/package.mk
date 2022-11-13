44e59bb41cbdb1576a271292cbb47e6433d82260
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:31:38 +0100

    tyrquake: update to git 89f3e03

--- packages/supervisedthinking/emulation/libretro/idtech/tyrquake/package.mk
+++ packages/supervisedthinking/emulation/libretro/idtech/tyrquake/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="tyrquake"
-PKG_VERSION="c516c1fe4266e5f982703d509cc10ceb12167894"
-PKG_SHA256="6925305c2b7b5b46b4174d8588af6e6be483ae8187bb550748ad496c7e683ef4"
+PKG_VERSION="89f3e032757b9b56e8d62a528a4a2472e60631aa"
+PKG_SHA256="b7393238453b2c117b0639f9f9f5dd9e6f34aa35ee39af109eb7e75a58b429ad"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/tyrquake"
 PKG_URL="https://github.com/libretro/tyrquake/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Libretro port of Tyrquake (Quake 1 engine)"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto -sysroot"
@@ -19,5 +19,5 @@ PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
