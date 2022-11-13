3656ce15b2b3d37a1b784ebe48bf1fc08552e57e
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:38:13 +0100

    beetle-pce-fast: update to git cc248db

--- packages/supervisedthinking/emulation/libretro/pcengine/beetle-pce-fast/package.mk
+++ packages/supervisedthinking/emulation/libretro/pcengine/beetle-pce-fast/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="beetle-pce-fast"
-PKG_VERSION="eb57e0c21f082de9f0ba86f26b3c716a2ed18b14"
-PKG_SHA256="bfcc4a6d3cd4e3c7afb937abd4d0f63fbe98913c516da8ba4b60a4385bf5422e"
+PKG_VERSION="cc248db4d2f47d0f255fbc1a3c651df4beb3d835"
+PKG_SHA256="35ec999649e4211f97e95390e0329292bd31019984717b9a44f9acc737e96d96"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/beetle-pce-fast-libretro"
 PKG_URL="https://github.com/libretro/beetle-pce-fast-libretro/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Standalone port of Mednafen PCE Fast to libretro."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto -sysroot"
@@ -19,5 +19,5 @@ PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
