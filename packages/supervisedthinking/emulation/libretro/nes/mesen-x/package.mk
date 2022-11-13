c4edd828961e96bff872374174cad2a3c0db5abd
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:37:06 +0100

    mesen(-x): updated build

--- packages/supervisedthinking/emulation/libretro/nes/mesen-x/package.mk
+++ packages/supervisedthinking/emulation/libretro/nes/mesen-x/package.mk
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
-PKG_NAME="mesen"
+PKG_NAME="mesen-x"
 PKG_VERSION="1.0.0"
 PKG_SHA256="e7828ce179cb7ca9657a065dbb09be0b71036cd76fc6f7a743938ffb1a9afae9"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/NovaSquirrel/Mesen-X"
 PKG_URL="https://github.com/NovaSquirrel/Mesen-X/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto -sysroot"
