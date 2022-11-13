10ead1a61e4b7187b2e989e643987db410a50021
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:43:44 +0100

    2048: update to git 0edef01

--- packages/supervisedthinking/emulation/libretro/various/2048/package.mk
+++ packages/supervisedthinking/emulation/libretro/various/2048/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="2048"
-PKG_VERSION="20051e140346fb56376b59f41f28ef40f8ad7fb8"
-PKG_SHA256="6cccc6ddefcd065b13aa01881ad82966eb5377657a97757c172c4e82f955780f"
+PKG_VERSION="0edef01f4c2dd2cbd0bb66b888ca4b57fad297d1"
+PKG_SHA256="d8eec941029b85418da76eb49b3459617af12effe243876225e87739c374f0c8"
 PKG_LICENSE="Unlicense"
 PKG_SITE="https://github.com/libretro/libretro-2048"
 PKG_URL="https://github.com/libretro/libretro-2048/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Port of 2048 puzzle game to the libretro API."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto -sysroot"
@@ -18,10 +18,11 @@ PKG_LIBPATH="${PKG_LIBNAME}"
 PKG_MAKE_OPTS_TARGET="-f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/config/retroarch/playlists
   mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}
 
-  #create Retroarch Playlist
-  cp ${PKG_DIR}/config/*   ${INSTALL}/usr/config/retroarch/playlists
-  cp -v ${PKG_LIBPATH}    ${INSTALL}/usr/lib/libretro/
+post_makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/config/retroarch/playlists
+    cp -v ${PKG_DIR}/config/* ${INSTALL}/usr/config/retroarch/playlists
 }
