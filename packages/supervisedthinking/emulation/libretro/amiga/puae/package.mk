76220e5de0c059c384c3b40788692ffff45b15f9
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 15:32:37 +0100

    (p)uae: update to git 4fa4c695

--- packages/supervisedthinking/emulation/libretro/amiga/puae/package.mk
+++ packages/supervisedthinking/emulation/libretro/amiga/puae/package.mk
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
-PKG_NAME="uae"
-PKG_VERSION="4961be5b7c69789cf4842eb4b3b02a195a2740f9"
-PKG_SHA256="20f98f8a253de2a2584a25520800c42b35389831023282fe1a7fab6b582954af"
+PKG_NAME="puae"
+PKG_VERSION="4fa4c695606fa26dc55c53b791cf1a832af28a51"
+PKG_SHA256="e5879ebe8a2b18cdb2cfe2ac36f8851940101010a74435d291dfcfa3a60fa9cc"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/libretro-uae"
 PKG_URL="https://github.com/libretro/libretro-uae/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc capsimg"
 PKG_LONGDESC="Libretro wrapper for UAE emulator."
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="-sysroot"
+PKG_BUILD_FLAGS="+lto +speed -sysroot"
 
 PKG_LIBNAME="puae_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
@@ -18,10 +18,8 @@ PKG_LIBPATH="${PKG_LIBNAME}"
 PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
 pre_configure_target() {
-  CFLAGS+=" -fcommon"
   if [ "${ARCH}" = "arm" ]; then
     PKG_MAKE_OPTS_TARGET+=" platform=armv"
-
     # ARM NEON support
     if target_has_feature neon; then
       PKG_MAKE_OPTS_TARGET+="-neon"
@@ -31,5 +29,10 @@ pre_configure_target() {
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}
+
+post_makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/share/retroarch/bios
+    cp -vr ${PKG_BUILD}/sources/uae_data ${INSTALL}/usr/share/retroarch/bios/
 }
