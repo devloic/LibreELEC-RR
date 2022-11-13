08dc75f4e12a81b397db7fa66165b2d52697abcb
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:38:31 +0100

    beetle-pcfx: update to git 08632fc

--- packages/supervisedthinking/emulation/libretro/pcfx/beetle-pcfx/package.mk
+++ packages/supervisedthinking/emulation/libretro/pcfx/beetle-pcfx/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="beetle-pcfx"
-PKG_VERSION="bfc0954e14b261a04dcf8dbe0df8798f16ae3f3c"
-PKG_SHA256="84191d221bc51c2595f2a192a71b955f0a1e521abfe0f9b64d9e87365eeaa4fc"
+PKG_VERSION="08632fcbc039f70dbd6da5810db9dcc304d7fbde"
+PKG_SHA256="3568660c2a276eabcd83a54128e557a3981b67d07784961823269cbd4a4b41a3"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/beetle-pcfx-libretro"
 PKG_URL="https://github.com/libretro/beetle-pcfx-libretro/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Standalone port of Mednafen PCFX to libretro."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto -sysroot"
