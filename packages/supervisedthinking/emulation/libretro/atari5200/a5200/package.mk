61e7a1124657d5e2c364f73f21acd01f4c988219
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 15:33:59 +0100

    a5200: update to git b8f8571

--- packages/supervisedthinking/emulation/libretro/atari5200/a5200/package.mk
+++ packages/supervisedthinking/emulation/libretro/atari5200/a5200/package.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2022-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="a5200"
-PKG_VERSION="7eee43299d0abe28828dee2b86f100b066391866"
-PKG_SHA256="7cb11c04b1b67c07d47b0ffa4eed13de50efe4a72f971ae87c3cd5593a68a4b2"
+PKG_VERSION="b8f8571eb5c6f484fe6be9a3a895ffb162b08422"
+PKG_SHA256="ea7e8d424e345c5eee3caf30f297286e9a3298719890d6b1f4b47d2d11d48050"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/a5200"
 PKG_URL="https://github.com/libretro/a5200/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Port of Atari 5200 emulator for GCW0"
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="-sysroot"
+PKG_BUILD_FLAGS="+lto +speed -sysroot"
 
 PKG_LIBNAME="a5200_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
