0abebfbde615728a2aa44d59973aa1e179fdfd27
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:41:11 +0100

    bsnes-hd: update to git 0482170

--- packages/supervisedthinking/emulation/libretro/snes/bsnes-hd/package.mk
+++ packages/supervisedthinking/emulation/libretro/snes/bsnes-hd/package.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="bsnes-hd"
-PKG_VERSION="65f24e56c37f46bb752190024bd4058e64ad77d1"
-PKG_SHA256="fce28338704fb8251394e432f1d163b1a0530a939742bf9817a448bb8476a4de"
+PKG_VERSION="04821703aefdc909a4fd66d168433fcac06c2ba7"
+PKG_SHA256="f3c5b4480c412af869fd0b050c80160308acdc5feb6a5c017dbcb9dd5fd6a725"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/DerKoun/bsnes-hd"
 PKG_URL="https://github.com/DerKoun/bsnes-hd/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="bsnes-hd is a fork of bsnes that adds HD video features."
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="+lto -sysroot"
+PKG_BUILD_FLAGS="+lto +speed -sysroot"
 
 PKG_LIBNAME="bsnes_hd_beta_libretro.so"
 PKG_LIBPATH="bsnes/out/${PKG_LIBNAME}"
@@ -27,5 +27,5 @@ configure_target(){
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
