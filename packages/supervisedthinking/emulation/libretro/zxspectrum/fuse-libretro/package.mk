ee4a07426d82568a81315fbfb367669b21dbe85a
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:45:25 +0100

    fuse-libretro: update to git 3f9344d

--- packages/supervisedthinking/emulation/libretro/zxspectrum/fuse-libretro/package.mk
+++ packages/supervisedthinking/emulation/libretro/zxspectrum/fuse-libretro/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fuse-libretro"
-PKG_VERSION="8b734a975633a0ed21494c2e1a50e7e1de432122"
-PKG_SHA256="5af46a0f21867501dd3268c2a7679ff60c545c9ea564206e31e657bbf5b298c5"
+PKG_VERSION="3f9344ddf88dfd251d95cffdea615692479e8cc6"
+PKG_SHA256="5b4f1b5d2eaacca013a46ed9a7105162500d15ed9d25da3f88b212226cd999e2"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/fuse-libretro"
 PKG_URL="https://github.com/libretro/fuse-libretro/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="fuse-libretro is an work in progress port of the Fuse Unix Spectrum Emulator to libretro."
 PKG_BUILD_FLAGS="-sysroot"
 
@@ -16,5 +16,5 @@ PKG_LIBPATH="${PKG_LIBNAME}"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
