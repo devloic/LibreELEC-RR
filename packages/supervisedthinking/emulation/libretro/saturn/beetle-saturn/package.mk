e92bc069417c4aadc0e298d9313e94e205709846
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:39:55 +0100

    beetle-saturn: update to git 054862a

--- packages/supervisedthinking/emulation/libretro/saturn/beetle-saturn/package.mk
+++ packages/supervisedthinking/emulation/libretro/saturn/beetle-saturn/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="beetle-saturn"
-PKG_VERSION="ae30f29e340a00b33e38df85ceaa599151a47cd7"
-PKG_SHA256="32abf768d707f0fb4ba75201c41db52af784c9828f3efebf2a09a4b26aaa28c2"
+PKG_VERSION="054862a4ccb9b2f1bad9e5b075fc3d1116dc8055"
+PKG_SHA256="de8cecbca299dd7bf185983a00307cb3bd429a6c80222a9377063c8e35400d03"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/beetle-saturn-libretro"
 PKG_URL="https://github.com/libretro/beetle-saturn-libretro/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Standalone port of Mednafen Saturn to the libretro API."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto -sysroot"
@@ -25,5 +25,5 @@ pre_configure_target() {
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
