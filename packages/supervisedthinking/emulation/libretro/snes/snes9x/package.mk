e5c9d1db5bfc8b798f3ee9a1085779ea82ab04d4
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-13 16:42:28 +0100

    snes9x: update to git 2ed8d68

--- packages/supervisedthinking/emulation/libretro/snes/snes9x/package.mk
+++ packages/supervisedthinking/emulation/libretro/snes/snes9x/package.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="snes9x"
-PKG_VERSION="ae16176a18fa2a7d642be5d66dbe1926d9d08e90"
-PKG_SHA256="54bb2a593820e9445784ae01e42e4fdc69a48c1c03346aa0406aa9622eea7dfc"
+PKG_VERSION="2ed8d687f199ad826b3d9cb527649c48784e12f8"
+PKG_SHA256="5e85faa79c9d75b2ee6f9d51b89cba923d2ea4e5a315eebb6f5ec39b4d048478"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/snes9x"
 PKG_URL="https://github.com/libretro/snes9x/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_DEPENDS_TARGET="toolchain glibc"
 PKG_LONGDESC="Snes9x - Portable Super Nintendo Entertainment System (TM) emulator"
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="+lto -sysroot"
+PKG_BUILD_FLAGS="+lto +speed -sysroot"
 
 PKG_LIBNAME="snes9x_libretro.so"
 PKG_LIBPATH="libretro/${PKG_LIBNAME}"
