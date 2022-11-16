cfa85ab3601879d439a33c4bc358d2e6f1969aa3
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-16 15:40:48 +0100

    slang-shaders-lr: update to git f77f7b3

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/slang-shaders-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/slang-shaders-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="slang-shaders-lr"
-PKG_VERSION="f8f99ee220edbf2c5df2714a73d46faea43e62e2"
-PKG_SHA256="d23912591e7294c8131f6b153fc39efb558e40e13057b563b92b903033c79986"
+PKG_VERSION="f77f7b3e3c1dd3c454ede05db7a46c476960a814"
+PKG_SHA256="f81a689475ab6fe420efb9fe8fc34915b2152ac8af455610b47531f26c918bbc"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/slang-shaders"
 PKG_URL="https://github.com/libretro/slang-shaders/archive/${PKG_VERSION}.tar.gz"
@@ -12,6 +12,5 @@ PKG_LONGDESC="Vulkan GLSL RetroArch shader system"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  cd ${PKG_BUILD}
-  make install INSTALLDIR="${INSTALL}/usr/share/retroarch/shaders/Vulkan-Shaders"
+  make -C ${PKG_BUILD} install INSTALLDIR="${INSTALL}/usr/share/retroarch/shaders/Vulkan-Shaders"
 }
