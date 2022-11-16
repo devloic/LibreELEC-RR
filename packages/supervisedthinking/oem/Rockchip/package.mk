3ebaa670e3d03622fe8455b1ffd1c863fc8e80ee
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-16 12:24:25 +0100

    oem/Projects: add jumpnbump

--- packages/supervisedthinking/oem/Rockchip/package.mk
+++ packages/supervisedthinking/oem/Rockchip/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="Rockchip"
-PKG_VERSION="1.0.6"
+PKG_VERSION="1.0.7"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://bit.ly/3vL5rH3"
 PKG_DEPENDS_TARGET="toolchain"
@@ -38,6 +38,7 @@ OEM_EMULATORS_LIBRETRO_ROCKCHIP=" \
   gambatte \
   genesis-plus-gx \
   genesis-plus-gx-wide \
+  jumpnbump \
   mame2003-plus \
   mame2010 \
   mame2016 \
