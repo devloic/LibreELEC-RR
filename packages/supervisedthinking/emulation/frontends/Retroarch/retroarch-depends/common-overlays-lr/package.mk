cb44f3b7dd65cd58c2492bdd24ab2ee8619bbb11
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-16 15:39:27 +0100

    common-overlays-lr: update to git b3827a2

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/common-overlays-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/common-overlays-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="common-overlays-lr"
-PKG_VERSION="e2ba730083769b45bac62d164115e7dc197c8b63"
-PKG_SHA256="e8b09b7c87242c0687a50bb5eab165003a8fc11e4cd3e5fdf83245d92e9c7f91"
+PKG_VERSION="b3827a2c63834ed9e1902acc7cf9019f64771ed3"
+PKG_SHA256="a19e8f78c3beea65954b445faa9aefa8b3f531acfd1b1ea6c5e4d0b848ea6314"
 PKG_LICENSE="CC-BY-4.0 License"
 PKG_SITE="https://github.com/libretro/common-overlays"
 PKG_URL="https://github.com/libretro/common-overlays/archive/${PKG_VERSION}.tar.gz"
@@ -12,6 +12,13 @@ PKG_LONGDESC="Collection of overlay files for use with libretro frontends, such 
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/share/retroarch/overlay
-  cp -r ${PKG_BUILD}/borders ${INSTALL}/usr/share/retroarch/overlay
+  make -C ${PKG_BUILD} install INSTALLDIR="${INSTALL}/usr/share/retroarch/overlays"
+}
+
+post_makeinstall_target() {
+  for PKG_OVERLAY_FILES in \
+    gamepads ipad keyboards misc wii
+  do
+    safe_remove ${INSTALL}/usr/share/retroarch/overlays/${PKG_OVERLAY_FILES}
+  done 
 }
