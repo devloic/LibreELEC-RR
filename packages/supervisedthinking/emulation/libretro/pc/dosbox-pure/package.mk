f55f39902b37ac8db6c7835edfc0a217767535f1
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-16 15:56:05 +0100

    emulation/libretro: clean up target dependency

--- packages/supervisedthinking/emulation/libretro/pc/dosbox-pure/package.mk
+++ packages/supervisedthinking/emulation/libretro/pc/dosbox-pure/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="1a6c0a48df884d76c53842845cdbe9ed835512facb73b72e80fd900868f8779a"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/schellingb/dosbox-pure"
 PKG_URL="https://github.com/schellingb/dosbox-pure/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain glibc"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
 PKG_BUILD_FLAGS="+lto +speed -sysroot"
 
