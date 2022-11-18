c426e72c61267319669ab1d16e2866966fa67967
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-18 02:40:18 +0100

    soundtouch: update to 2.3.2

--- packages/supervisedthinking/audio/soundtouch/package.mk
+++ packages/supervisedthinking/audio/soundtouch/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="soundtouch"
-PKG_VERSION="2.3.1"
-PKG_SHA256="42633774f372d8cb0a33333a0ea3b30f357c548626526ac9f6ce018c94042692"
+PKG_VERSION="2.3.2"
+PKG_SHA256="ed714f84a3e748de87b24f385ec69d3bdc51ca47b7f4710d2048b84b2761e7ff"
 PKG_LICENSE="LGPL-2.1"
 PKG_SITE="https://codeberg.org/soundtouch/soundtouch"
 PKG_URL="https://codeberg.org/soundtouch/soundtouch/archive/${PKG_VERSION}.tar.gz"
