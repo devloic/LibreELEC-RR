703c4885c91da227bcfe41be0b8aefd63a43e3d7
SupervisedThinking <supervisedthinking@gmail.com>
2022-11-20 01:13:12 +0100

    core-info-lr: update to 1.13.0

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/core-info-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/core-info-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="core-info-lr"
-PKG_VERSION="1.12.0"
-PKG_SHA256="a8f18209d4448abd5c4bff77fcc2fbdf06cf2f248fb84f898591b4a3314f7141"
+PKG_VERSION="1.13.0"
+PKG_SHA256="e0b68b97a7fb9b41dc460588eb9311583a8080074c92159303afc03ad1dacf9a"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-core-info"
 PKG_URL="https://github.com/libretro/libretro-core-info/archive/refs/tags/v${PKG_VERSION}.tar.gz"
@@ -12,6 +12,5 @@ PKG_LONGDESC="Libretro's core info files"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  cd ${PKG_BUILD}
-  make install INSTALLDIR="${INSTALL}/usr/share/retroarch/coreinfo"
+  make -C ${PKG_BUILD} install INSTALLDIR="${INSTALL}/usr/share/retroarch/coreinfo"
 }
