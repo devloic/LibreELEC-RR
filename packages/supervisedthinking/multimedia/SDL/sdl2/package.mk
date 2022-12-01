4ddba1a43c3cd727f6df3df4aa389aa625ca4e52
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-01 23:31:51 +0100

    sdl2: update to 2.26.1

--- packages/supervisedthinking/multimedia/SDL/sdl2/package.mk
+++ packages/supervisedthinking/multimedia/SDL/sdl2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="sdl2"
-PKG_VERSION="2.26.0"
-PKG_SHA256="f38367892a6f243e8b4010e9e3d9714dc848e11b1a3f69c4af514dc6e7aca7f0"
+PKG_VERSION="2.26.1"
+PKG_SHA256="91d8d26fc45a27c215b70090b351fd30e36bfe44bdd940e5db13d5148058d226"
 PKG_LICENSE="SDL"
 PKG_SITE="https://www.libsdl.org/"
 PKG_URL="https://github.com/libsdl-org/SDL/archive/refs/tags/release-${PKG_VERSION}.tar.gz"
