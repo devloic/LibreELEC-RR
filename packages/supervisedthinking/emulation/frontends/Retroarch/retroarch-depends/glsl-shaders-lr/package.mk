5115d2f1feb7dbd78155881ab47ca4ebae0dd2cc
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-02 18:56:34 +0100

    glsl-shaders-lr: update to git 66cd391

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/glsl-shaders-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/glsl-shaders-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="glsl-shaders-lr"
-PKG_VERSION="46a00f15e09ffbd4fba46ab41f0653aeea918fd1"
-PKG_SHA256="ae449b1f82d6398b544e8c7f62a3e085ec5a3dd84dd75ff47bb55963c468b28d"
+PKG_VERSION="66cd3917c3cde8c96b5492bca00c95e3e17b44b9"
+PKG_SHA256="cb24da2427c643e2cc3e1562235680f74dffd7c9adddf098ea4deeb9158fa20a"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/glsl-shaders"
 PKG_URL="https://github.com/libretro/glsl-shaders/archive/${PKG_VERSION}.tar.gz"
