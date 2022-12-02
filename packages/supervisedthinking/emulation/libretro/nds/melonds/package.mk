6558c07fb4f29b2b8cfe72d3a69f60058a0d9f14
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-02 18:55:14 +0100

    melonds: update to git 1eebc82

--- packages/supervisedthinking/emulation/libretro/nds/melonds/package.mk
+++ packages/supervisedthinking/emulation/libretro/nds/melonds/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="melonds"
-PKG_VERSION="5e52c245fb38cabe881fbfa6513280ee44fc5bd8"
-PKG_SHA256="a487a18d1a39b24d2f339816155054f24532bbaa25a6c39af5feb88fde5afb6a"
+PKG_VERSION="1eebc828769f2619bd79f39545573a991391de5d"
+PKG_SHA256="7290ef39fae3db2ad905cccccde85943466998dc409daa7e8a58beaa4c14b4a5"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/melonDS"
 PKG_URL="https://github.com/libretro/melonDS/archive/${PKG_VERSION}.tar.gz"
