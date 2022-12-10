f03ec4965629ec7708f3cbc34b40fcce4321d715
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-10 13:45:24 +0100

    linux: update Amlogic to 6.0.12

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -16,8 +16,8 @@ PKG_PATCH_DIRS="${LINUX}"
 
 case "${LINUX}" in
   amlogic)
-    PKG_VERSION="6.0.11"
-    PKG_SHA256="96c4b07fafb43dfb1350d8370b291f651f574cb4869b82d4ae786263a7a33c83"
+    PKG_VERSION="6.0.12"
+    PKG_SHA256="4edcf42204a68c13dc9b6bb4fe994fb9f6096b15fa960c14357fb3864f644912"
     PKG_URL="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/${PKG_NAME}-${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
