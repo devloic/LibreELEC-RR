ee51b7b6c6c889b7381e29c44bfdd848218c6e19
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-13 00:01:21 +0100

    pixel-es-theme: update to git 5d12229

--- packages/supervisedthinking/emulation/frontends/Emulationstation/emulationstation-depends/pixel-es-theme/package.mk
+++ packages/supervisedthinking/emulation/frontends/Emulationstation/emulationstation-depends/pixel-es-theme/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="pixel-es-theme"
-PKG_VERSION="c00c9451be00d3a1c19fb51ca55c240ffda462d2"
-PKG_SHA256="7231e243a3c4346c006890b536a99ce4731bbba3be153a9e4ed8c5b1fe27fcd0"
+PKG_VERSION="5d12229eb587af05d546ed963cabc9ddb5ab926b"
+PKG_SHA256="9fd801f4a85c140f5a85647fea6ac84bbe74b3504a2bd8b7dac6a094ac61fca2"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/SupervisedThinking/es-theme-pixel"
 PKG_URL="https://github.com/SupervisedThinking/es-theme-pixel/archive/${PKG_VERSION}.tar.gz"
