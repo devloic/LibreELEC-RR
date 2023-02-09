d6486cb59eefa9720692ab633a5bb649a6d817a7
SupervisedThinking <supervisedthinking@gmail.com>
2023-02-09 21:35:13 +0100

    wxwidgets: update to 3.2.2

--- packages/supervisedthinking/toolkit/wxwidgets/package.mk
+++ packages/supervisedthinking/toolkit/wxwidgets/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="wxwidgets"
-PKG_VERSION="3.2.1"
-PKG_SHA256="c229976bb413eb88e45cb5dfb68b27890d450149c09b331abd751e7ae0f5fa66"
+PKG_VERSION="3.2.2"
+PKG_SHA256="8edf18672b7bc0996ee6b7caa2bee017a9be604aad1ee471e243df7471f5db5d"
 PKG_LICENSE="wxWidgets licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="https://github.com/wxWidgets/wxWidgets/releases/download/v${PKG_VERSION}/wxWidgets-${PKG_VERSION}.tar.bz2"
